df3920af055b31bd4b6c1dc9683b62efeaee9e62
Anders Ryden <rydena021@gmail.com>
2018-11-12 10:08:39 -0600

    changes delete button IDs to classes

--- client.js
+++ client.js
@@ -1,8 +1,8 @@
 class Employee {
-  constructor(firstName, lastName, employeeID, title, annualSalary) {
+  constructor(firstName, lastName, employeeId, title, annualSalary) {
     this.firstName = firstName;
     this.lastName = lastName;
-    this.employeeID = employeeID;
+    this.employeeId = employeeId;
     this.title = title;
     this.annualSalary = annualSalary;
     this.monthlySalary = annualSalary / 12;
@@ -20,14 +20,9 @@ let monthlyBudget = 20000;
 
 $(readyNow);
 
-function readyNow() {
-  $('#addEmployeeButton').on('click', addEmployee);
-  displayEmployees();
-  updateBudget();
-} // end readyNow
 
 function addEmployee() {
-  if ($('#firstNameIn').val() === '' || $('#lastNameIn').val() === '' || $('#employeeIDIn').val() === '' || $('#titleIn').val() === '' || $('#annualSalaryIn').val() === '') {
+  if ($('#firstNameIn').val() === '' || $('#lastNameIn').val() === '' || $('#employeeIdIn').val() === '' || $('#titleIn').val() === '' || $('#annualSalaryIn').val() === '') {
     alert("All fields mandatory.");
   } else if (!idIsValid()) {
     alert("Please enter a unique 4-digit employee ID.");
@@ -35,32 +30,32 @@ function addEmployee() {
     const newEmployee = new Employee(
       $('#firstNameIn').val(),
       $('#lastNameIn').val(),
-      parseInt($('#employeeIDIn').val()),
+      parseInt($('#employeeIdIn').val()),
       $('#titleIn').val(),
       parseInt($('#annualSalaryIn').val())
-    );
-    // reset input fields
-    $('#firstNameIn').val('');
-    $('#lastNameIn').val('');
-    $('#employeeIDIn').val('');
-    $('#titleIn').val('');
-    $('#annualSalaryIn').val('');
-    // push to employees array
-    employees.push(newEmployee);
-    // dislay all employees
-    displayEmployees();
-    updateBudget();
-  }
-} // end addEmployee
+      );
+      // reset input fields
+      $('#firstNameIn').val('');
+      $('#lastNameIn').val('');
+      $('#employeeIdIn').val('');
+      $('#titleIn').val('');
+      $('#annualSalaryIn').val('');
+      // push to employees array
+      employees.push(newEmployee);
+      // dislay all employees
+      displayEmployees();
+      updateBudget();
+    }
+  } // end addEmployee
 
 function currencyFormat(num) {
-  return '$' + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+  return '$' + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
 } // end currencyFormat
 
 function deleteEmployee() {
-  let employeeID = $(this).closest('tr').find("td.id").text();
+  let employeeId = $(this).closest('tr').find("td.id").text();
   for (let i = 0; i < employees.length; i++) {
-    if (employeeID == employees[i].employeeID) {
+    if (employeeId == employees[i].employeeId) {
       employees.splice(i, 1);
     }
   }
@@ -71,28 +66,34 @@ function deleteEmployee() {
 function displayEmployees() {
   $('tbody').empty();
   for (const employee of employees) {
-    $('tbody').append(`<tr>
-      <td>${employee.firstName}</td>
-      <td>${employee.lastName}</td>
-      <td class="id">${employee.employeeID}</td>
-      <td>${employee.title}</td>
-      <td>${currencyFormat(employee.annualSalary)}</td>,
-      <td><a class="btn" id="deleteEmployeeButton"><i class="fas fa-trash-alt"></i></a></td>
-      </tr>`);
+    $('tbody').append(`
+    <tr>
+    <td>${employee.firstName}</td>
+    <td>${employee.lastName}</td>
+    <td class="id">${employee.employeeId}</td>
+    <td>${employee.title}</td>
+    <td>${currencyFormat(employee.annualSalary)}</td>,
+    <td><a class="btn deleteEmployeeButton"><i class="fas fa-trash-alt"></i></a></td>
+    </tr>
+    `);
   }
-  // remove all listeners and reapply
-  $(document).off('click', "#deleteEmployeeButton", deleteEmployee);
-  $(document).on('click', "#deleteEmployeeButton", deleteEmployee);
+  $('.deleteEmployeeButton').on('click', deleteEmployee);
 } // end displayEmployees
 
 function idIsValid() {
   for (const employee of employees) {
-    if ($('#employeeIDIn').val() == employee.employeeID || $('#employeeIDIn').val().length != 4) {
+    if ($('#employeeIdIn').val() == employee.employeeId || $('#employeeIdIn').val().length != 4) {
       return false;
     }
   }
   return true;
 } // end idIsValid
+
+function readyNow() {
+  $('#addEmployeeButton').on('click', addEmployee);
+  displayEmployees();
+  updateBudget();
+} // end readyNow
 
 function updateBudget() {
   let totalCost = 0;
